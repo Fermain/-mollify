d5cab208238b843ebfd6b17e62896f69451c9ba9
pretzL <ericpretzinger@gmail.com>
2023-04-18 22:20:03 +0200

    Structure like an actual page

--- packages/lms/src/routes/+page.svelte
+++ packages/lms/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style lang="scss">
+	main {
+		max-width: 80ch;
+		margin: 0 auto;
+	}
+</style>
