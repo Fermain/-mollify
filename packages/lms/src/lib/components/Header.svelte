2b889d5acf2dcac7176fea92f9b1f46508b5a089
pretzL <ericpretzinger@gmail.com>
2023-04-18 22:20:15 +0200

    Create initial Header component

---
+++ packages/lms/src/lib/components/Header.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	let fontSize: number = 16;
+	let themeChoice: string;
+	let searchValue: string;
+</script>
+
+<header>
+	<h1>Mollify</h1>
+	<div class="settings">
+		<div class="font-size-selector">
+			<span
+				id="font-plus"
+				on:click={() => {
+					fontSize++;
+				}}>A +</span
+			>
+			<span
+				id="font-minus"
+				on:click={() => {
+					fontSize--;
+				}}
+			>
+				A -</span
+			>
+		</div>
+		<label>Theme</label>
+		<input type="search" bind:value={searchValue} placeholder="Search..." />
+	</div>
+</header>
+
+<style lang="scss">
+	header {
+		display: flex;
+		align-items: center;
+		width: 100%;
+		justify-content: space-between;
+		max-width: 80ch;
+		margin: 0 auto;
+	}
+
+	.settings {
+		display: flex;
+		gap: 1rem;
+	}
+</style>
