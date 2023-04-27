fb8fb33d93a81c2600240e19f0db6080284ed843
Fermain <Fermain@users.noreply.github.com>
2023-04-27 08:05:12 +0200

    Rework of the migrate command

--- packages/cli/src/commands/migrate/index.ts
+++ packages/cli/src/commands/migrate/index.ts
@@ -1,49 +1,47 @@
 import { Command } from 'commander';
-import { prompt } from 'enquirer';
-import { EntityType } from '../../types';
-import listMarkdown from '../../actions/listMarkdown';
-import migrateEntity from '../../actions/migrateEntity';
+import glob from 'glob';
+import path from 'path';
+import fs from 'fs-extra';
+import slugify from 'slugify';
 
-async function migrateEntityPrompt(entityType: EntityType, basePath = '') {
-  const files = await listMarkdown(basePath);
+async function migrateFiles(basePath: string) {
+  const pattern = path.join(basePath, '**/!(*+page).md');
+  const ignorePattern = path.join('src', 'templates', '**/*'); // Ignore pattern for src/templates
+  const files = glob.sync(pattern, { ignore: ignorePattern });
 
-  const { originPath, name } = await prompt<{
-    originPath: string;
-    name: string;
-  }>([
-    {
-      type: 'autocomplete',
-      name: 'originPath',
-      message: `Which ${entityType} do you want to migrate?`,
-      choices: files,
-      initial: 0,
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: `What is the name of the ${entityType}?`,
-      validate(input) {
-        return input.length > 0;
-      },
-    },
-  ]);
+  for (const file of files) {
+    const newDirName = slugify(path.basename(path.dirname(file)), {
+      strict: true,
+      lower: true,
+    });
+    const newDir = path.join(path.dirname(file), '..', newDirName);
+    const newFilePath = path.join(newDir, '+page.md');
 
-  await migrateEntity(originPath, entityType, name);
+    if (!fs.existsSync(newFilePath)) {
+      await fs.ensureDir(newDir);
+      await fs.move(file, newFilePath);
+      console.log(`Migrated ${file} -> ${newFilePath}`);
+    } else {
+      console.log(
+        `Skipped ${file} as there's already a +page.md file in the folder.`,
+      );
+    }
+  }
 }
 
 export default new Command('migrate')
-  .arguments('<entity-type>')
-  .description('Migrate a markdown file to an entity')
-  .action((entityType: EntityType) => {
-    if (!Object.values(EntityType).includes(entityType)) {
-      console.error(`Invalid entity type: ${entityType}`);
-      process.exit(1);
-    }
+  .arguments('[basePath]')
+  .description('Migrate markdown files to the desired structure')
+  .action(async (basePath = '') => {
+    const filesToMigrate = glob.sync(path.join(basePath, '**/!(*+page).md'), {
+      ignore: path.join('src', 'templates', '**/*'),
+    }).length;
 
-    migrateEntityPrompt(entityType)
-      .then(() => console.log('Entity migrated'))
-      .catch((error) => {
-        console.error(`Error migrating entity: ${error.message}`);
-        process.exit(1);
-      });
+    if (filesToMigrate > 0) {
+      console.log(`Migrating ${filesToMigrate} markdown files...`);
+      await migrateFiles(basePath);
+      console.log('Migration completed');
+    } else {
+      console.log('No markdown files found for migration');
+    }
   });
