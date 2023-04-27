7b2d9968b631959e99ef2afa642a818aee52127c
Eric Pretzinger <ericpretzinger@gmail.com>
2023-04-27 10:55:48 +0200

    Suggestion for basic implementation of sending messages
    Devoid of actual styling so we need to make sure we get the global styles going and then tweak this

--- packages/molly/src/routes/chat/components/chatMessage.svelte
+++ packages/molly/src/routes/chat/components/chatMessage.svelte
@@ -1,54 +1,26 @@
-<script lang=ts>
-  import { onMount } from 'svelte';
-  import Logo from './chatLogo.svelte';
-
-  onMount(() => {
-    //Scroll to the bottom of the chat window
-    const chatWindow: HTMLElement | null = document.querySelector('.chat-content');
-    if (chatWindow) {
-      chatWindow.scrollTop = chatWindow.scrollHeight;
-    }
-  });
-
-  //Watch for changes in the messages array and scroll to the bottom of the chat window
-  $: {
-    const chatWindow: HTMLElement | null = document.querySelector('.chat-content');
-    if (chatWindow) {
-      chatWindow.scrollTop = chatWindow.scrollHeight;
-    }
-  }
-
+<script lang="ts">
+	export let message: Object;
 </script>
 
-<div class="chat-content">
-  <div class="greeting">
-    <Logo />
-   <span>Hello, what can I do for you?</span>
-  </div>
-
+<div class="message-wrapper">
+	<strong>{message.name}</strong>
+	<p>{message.content}</p>
 </div>
 
-<style>
-    
-  .chat-content {
-  flex-grow: 1;
-  overflow-y: auto;
-  }
-  
-  .greeting {
-  color: white;
-  max-width: 100%;
-  margin: 0;
-  margin: 5px 0px 5px 0px;
-  background-color: #323e47e8;
-  cursor: default; 
-  padding: 10px;
-  display: flex; 
-  }
+<style lang="scss">
+	.message-wrapper {
+		display: flex;
+		flex-direction: column;
+		color: white;
+		max-width: 100%;
+		margin: 0;
+		background-color: #323e47e8;
+		cursor: default;
+		padding: 10px;
+	}
 
-
-  span {
-    margin-left: 10px;
-  }
-  
+	p {
+		padding: 0;
+		margin: 0;
+	}
 </style>
--- packages/molly/src/routes/chat/mollyChat.svelte
+++ packages/molly/src/routes/chat/mollyChat.svelte
@@ -1,76 +1,72 @@
-<script lang="ts"> 
-  import Logo from './components/chatLogo.svelte';
-  import AskMolly from './components/chatHeading.svelte';
-  import ChatInput from './components/chatInput.svelte';
-  import ChatMessages from './components/chatMessage.svelte';
-  import ChatButton from './components/chatButton.svelte';
-  import ChatBubble from './components/chatBubble.svelte';
-  export let expanded: boolean = false;  
+<script lang="ts">
+	import Logo from './components/chatLogo.svelte';
+	import AskMolly from './components/chatHeading.svelte';
+	import ChatInput from './components/chatInput.svelte';
+	import ChatFeed from './components/chatFeed.svelte';
+	import ChatButton from './components/chatButton.svelte';
+	import ChatBubble from './components/chatBubble.svelte';
+
+	export let expanded: boolean = false;
+
+	let messages: Array<Object> = [];
+
+	function handleChatInputMessage(event: CustomEvent<Message>) {
+		// Add the message payload to the messages array
+		messages = [...messages, event.detail];
+	}
 </script>
-  
-<style>
-    
-  .container {
-    position: fixed;
-    bottom: 0;
-    right: 0;
-    width: 300px;
-    height: 400px;
-    background-color: #D9D9D9;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .chat-header {
-    background-color: #323E47;
-    padding: 10px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
 
-  .chat-molly{
-    display: flex;
-    flex-direction: row;
-  }
+<!--This is the "chat-bubble" or the "molly-bubble"-->
+<ChatBubble expandButton={() => (expanded = !expanded)} />
 
-  </style>
-
-
-  <!--This is the "chat-bubble" or the "molly-bubble"-->
- <ChatBubble expandButton={() => expanded = !expanded}/>
-  
-  <!--This is the div that displays the chat window
+<!--This is the div that displays the chat window
   
   this is located down in the right side of the page when the chat bubble is pressed-->
-  
-  {#if expanded}
-  
-    <div class="container">
-    
-    <div class="chat-header">
-    
-    <div class="chat-molly">
-    
-      <Logo />
-    
-      <AskMolly />
-    
-    </div>
-    
-    <div class="right-side-header">
-    
-    <ChatButton expandButton={() => expanded = !expanded}/>
-    
-    </div>
-    
-    </div>
 
-      <ChatMessages />
-    
-      <ChatInput />
-    
-    </div>
-  
-  {/if}
+{#if expanded}
+	<div class="container">
+		<div class="chat-header">
+			<div class="chat-molly">
+				<Logo />
+
+				<AskMolly />
+			</div>
+
+			<div class="right-side-header">
+				<ChatButton expandButton={() => (expanded = !expanded)} />
+			</div>
+		</div>
+
+		<ChatFeed data={messages} />
+
+		<!-- Add the "on:message" event listener to the ChatInput component -->
+		<ChatInput on:message={handleChatInputMessage} />
+	</div>
+{/if}
+
+<style>
+	.container {
+		position: fixed;
+		bottom: 0;
+		right: 0;
+		width: 300px;
+		height: 400px;
+		background-color: #d9d9d9;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+		display: flex;
+		flex-direction: column;
+	}
+
+	.chat-header {
+		background-color: #323e47;
+		padding: 10px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	.chat-molly {
+		display: flex;
+		flex-direction: row;
+	}
+</style>
