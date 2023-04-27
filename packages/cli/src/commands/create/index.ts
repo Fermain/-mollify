4eb76d4e286ad30158ba2554ce22da27d76a13a3
Fermain <Fermain@users.noreply.github.com>
2023-04-27 04:40:25 +0200

    Improvements to create command

--- packages/cli/src/commands/create/index.ts
+++ packages/cli/src/commands/create/index.ts
@@ -4,31 +4,57 @@ import slugify from 'slugify';
 import { EntityType, Entity } from '../../types';
 import createEntity from '../../actions/createEntity';
 
-async function createEntityPrompt(entityType: EntityType, destination = '', passedTitle?: string, passedSlug?: string) {
-  const title = passedTitle ?? (await prompt<{ title: string }>([
-    {
-      type: 'input',
-      name: 'title',
-      message: `What is the name of the ${entityType}?`,
-      required: true,
-      validate(input) {
-        return input.length > 0;
-      },
-    },
-  ])).title;
+async function createEntityPrompt(
+  entityType?: EntityType,
+  destination = '',
+  passedTitle?: string,
+  passedSlug?: string,
+) {
+  entityType =
+    entityType ??
+    (
+      await prompt<{ entityType: EntityType }>([
+        {
+          type: 'select',
+          name: 'entityType',
+          message: 'Which entity type do you want to create?',
+          choices: Object.values(EntityType),
+        },
+      ])
+    ).entityType;
 
-  const slug = passedSlug ?? (await prompt<{ slug: string }>([
-    {
-      type: 'input',
-      name: 'slug',
-      required: true,
-      validate(input) {
-        return input.length > 0;
-      },
-      message: `What is the slug of the ${entityType}?`,
-      initial: slugify(title, { lower: true, strict: true }),
-    },
-  ])).slug;
+  const title =
+    passedTitle ??
+    (
+      await prompt<{ title: string }>([
+        {
+          type: 'input',
+          name: 'title',
+          message: `What is the name of the ${entityType}?`,
+          required: true,
+          validate(input) {
+            return input.length > 0;
+          },
+        },
+      ])
+    ).title;
+
+  const slug =
+    passedSlug ??
+    (
+      await prompt<{ slug: string }>([
+        {
+          type: 'input',
+          name: 'slug',
+          required: true,
+          validate(input) {
+            return input.length > 0;
+          },
+          message: `What is the slug of the ${entityType}?`,
+          initial: slugify(title, { lower: true, strict: true }),
+        },
+      ])
+    ).slug;
 
   const entity: Entity = {
     title,
@@ -40,18 +66,20 @@ async function createEntityPrompt(entityType: EntityType, destination = '', pass
 }
 
 export default new Command('create')
-  .arguments('<entity-type> [destination] [title] [slug]')
+  .arguments('[destination] [title] [slug] [entity-type]')
   .description('Create a new entity')
-  .action((entityType: EntityType, destination: string, title?: string, slug?: string) => {
-    if (!Object.values(EntityType).includes(entityType)) {
-      console.error(`Invalid entity type: ${entityType}`);
-      process.exit(1);
-    }
-
-    createEntityPrompt(entityType, destination, title, slug)
-      .then(() => console.log('Entity created'))
-      .catch((error) => {
-        console.error(`Error creating entity: ${error.message}`);
-        process.exit(1);
-      });
-  });
+  .action(
+    (
+      destination: string,
+      title?: string,
+      slug?: string,
+      entityType?: EntityType,
+    ) => {
+      createEntityPrompt(entityType, destination, title, slug)
+        .then(() => console.log('Entity created'))
+        .catch((error) => {
+          console.error(`Error creating entity: ${error.message}`);
+          process.exit(1);
+        });
+    },
+  );
