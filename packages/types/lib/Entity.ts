3286b72ff93ace64f956ced5e4a50c7d9e158cc9
Fermain <Fermain@users.noreply.github.com>
2023-05-02 11:12:02 +0200

    Updates types with Entity class

--- packages/types/lib/Entity.ts
+++ packages/types/lib/Entity.ts
@@ -1,3 +1,11 @@
+import path from 'path';
+
+// Note: this is a stub implementation
+function getChildrenSync(path: string) {
+  console.log(`getChildrenSync(${path}) not implemented.`);
+  return new Array<Entity>()
+}
+
 export enum EntityType {
   Institution = 'institution',
   Programme = 'programme',
@@ -8,16 +16,34 @@ export enum EntityType {
 }
 
 // Lightweight metadata object for an entity
-export interface Entity {
+export interface EntityBase {
   type: EntityType;
   title: string;
-  slug: string;
   tags: Array<string>;
+  previous?: string;
 }
 
-// Represents an entity directory on disk
-export interface EntityContent extends Entity {
-  path: string;
-  body: string;
-  children: Array<EntityContent>;
+export interface EntityMeta extends EntityBase {
+  slug: string;
+  children: Array<EntityMeta>;
+  address: string;
+}
+
+export class Entity implements EntityMeta {
+  public slug: string;
+  public children = new Array<EntityMeta>();
+
+  constructor(
+    public address: string,
+    public type: EntityType,
+    public title: string,
+    public tags: Array<string>,
+    public previous?: string,
+  ) {
+    // Slug truth comes from the disk path
+    this.slug = path.dirname(address);
+
+    // Entity child truth comes from the disk path
+    this.children = getChildrenSync(address);
+  }
 }
