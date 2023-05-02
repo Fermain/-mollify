cc208e9dc847694c7d16cfae33168feee5aa6bba
Fermain <Fermain@users.noreply.github.com>
2023-05-02 11:35:28 +0200

    Simplifies the Entity class

--- packages/types/lib/Entity.ts
+++ packages/types/lib/Entity.ts
@@ -1,21 +1,35 @@
-import path from 'path';
+import path from "path";
 
-// Note: this is a stub implementation
+// Stub implementation for retrieving child entities from disk
 function getChildrenSync(path: string) {
   console.log(`getChildrenSync(${path}) not implemented.`);
-  return new Array<Entity>()
+  return new Array<Entity>();
 }
 
+// Stub implementation for retrieving entity metadata from disk
+function getEntityMetaSync(path: string): EntityMeta {
+  console.log(`getEntityMetaSync(${path}) not implemented.`);
+  return {
+    type: EntityType.Course,
+    title: "Test Course",
+    tags: new Array<string>(),
+    slug: "test-course",
+    children: [],
+    address: path,
+  };
+}
+
+// Enumeration of entity types
 export enum EntityType {
-  Institution = 'institution',
-  Programme = 'programme',
-  Course = 'course',
-  Module = 'module',
-  Lesson = 'lesson',
-  Assessment = 'assessment'
+  Institution = "institution",
+  Programme = "programme",
+  Course = "course",
+  Module = "module",
+  Lesson = "lesson",
+  Assessment = "assessment",
 }
 
-// Lightweight metadata object for an entity
+// Base interface for entity metadata
 export interface EntityBase {
   type: EntityType;
   title: string;
@@ -23,27 +37,38 @@ export interface EntityBase {
   previous?: string;
 }
 
+// Extended interface for entity metadata, including slug, children, and address
 export interface EntityMeta extends EntityBase {
   slug: string;
   children: Array<EntityMeta>;
   address: string;
 }
 
+/**
+ * Entity class - represents a single content file with all additional metadata
+ * @example
+ * const testCourse = new Entity('content/courses/test-course');
+ */
 export class Entity implements EntityMeta {
-  public slug: string;
-  public children = new Array<EntityMeta>();
+  public readonly slug: string;
+  public readonly children = new Array<EntityMeta>();
+  public readonly type: EntityType;
+  public readonly title: string;
+  public readonly tags = new Array<string>();
+  public readonly previous?: string;
 
-  constructor(
-    public address: string,
-    public type: EntityType,
-    public title: string,
-    public tags: Array<string>,
-    public previous?: string,
-  ) {
-    // Slug truth comes from the disk path
+  constructor(public readonly address: string) {
+    // Assign the slug based on the directory name from the given address
     this.slug = path.dirname(address);
 
-    // Entity child truth comes from the disk path
+    // Retrieve entity metadata from disk
+    const { type, title, tags, previous } = getEntityMetaSync(address);
+    this.type = type;
+    this.title = title;
+    this.tags = tags;
+    this.previous = previous;
+
+    // Retrieve child entities from disk
     this.children = getChildrenSync(address);
   }
 }
