635aee7267f563050c16ebc0d44db45044721a5e
Fermain <Fermain@users.noreply.github.com>
2023-05-03 11:30:08 +0200

    submodule link wip

--- packages/cli/src/commands/link/index.ts
+++ packages/cli/src/commands/link/index.ts
@@ -1,3 +1,15 @@
-// Command to create a symlink between a local package and a dependency
+import { Command } from 'commander';
+import { linkEntity } from '../../actions/linkEntity';
 
-export default null;
+export function register(program: Command) {
+  program
+    .command('link')
+    .description('Add a remote repository as a git submodule in the root directory')
+    .action(async () => {
+      try {
+        await linkEntity();
+      } catch (error) {
+        console.error(`Error: ${error}`);
+      }
+    });
+}
