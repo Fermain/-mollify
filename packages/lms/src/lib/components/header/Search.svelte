bc7fbd575049e317dc8a3a389030ae600f43371f
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-05-04 20:55:24 +0200

    added basic searching

--- packages/lms/src/lib/components/header/Search.svelte
+++ packages/lms/src/lib/components/header/Search.svelte
@@ -1,11 +1,53 @@
+<script lang="ts">
+	let searchQuery = '';
+	let searchResults = [];
+
+	async function search() {
+		const query = `
+    query SearchMarkdown($filter: String) {
+      searchMarkdown(filter: $filter) {
+        title
+        browserPath
+      }
+    }
+  `;
+
+		const variables = {
+			filter: searchQuery
+		};
+
+		const response = await fetch('/api/search', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ query, variables })
+		});
+
+		const data = await response.json();
+		console.log(data);
+		if (errors) {
+			console.log(error);
+			console.error('GraphQL errors:', errors);
+		} else {
+			searchResults = data.searchMarkdown;
+		}
+	}
+</script>
+
 <form class="search">
-  <input type="search" placeholder="Search" />
-  <button>Search</button>
+	<input
+		type="search"
+		placeholder="Search markdown content"
+		bind:value={searchQuery}
+		on:input={search}
+	/>
+	<button>Search</button>
 </form>
 
 <style>
-  .search {
-    display: flex;
-    flex: 1;
-  }
+	.search {
+		display: flex;
+		flex: 1;
+	}
 </style>
