51689a7162636541ea928dfd678b830792bd3a8b
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-05-04 10:21:25 +0200

    Styles scrollbar in main component

--- packages/lms/src/lib/components/content/Main.svelte
+++ packages/lms/src/lib/components/content/Main.svelte
@@ -13,4 +13,19 @@
 		grid-area: body;
 		overflow-y: auto;
 	}
+
+	::-webkit-scrollbar {
+		width: 6px;
+	}
+
+	::-webkit-scrollbar-track {
+		box-shadow: inset 0 0 4px var(--primary);
+		border-radius: var(--border-radius-xs);
+		background-color: var(--background-primary);
+	}
+
+	::-webkit-scrollbar-thumb {
+		background-color: var(--primary);
+		border-radius: var(--border-radius-xs);
+	}
 </style>
