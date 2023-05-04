c261bab4f2315f552413f7eb303d1329ee9143e1
Jenny Feragen <jenny@feragen.no>
2023-05-04 18:23:47 +0200

    removed boilerplate code

--- packages/molly/src/routes/+page.svelte
+++ packages/molly/src/routes/+page.svelte
@@ -2,7 +2,4 @@
 	import MollyChat from "./chat/mollyChat.svelte";
 
 </script>
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <MollyChat/>
