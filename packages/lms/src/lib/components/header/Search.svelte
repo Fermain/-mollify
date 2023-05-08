45fab1a27a4f8ab92484f44f96140034059ca531
Eric Pretzinger <ericpretzinger@gmail.com>
2023-05-08 14:05:47 +0200

    Adds lookahead search bar

--- packages/lms/src/lib/components/header/Search.svelte
+++ packages/lms/src/lib/components/header/Search.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { search } from '$lib/utils/graphql/getSearchResults';
+	import SearchItem from '../search/SearchItem.svelte';
+
 	let searchQuery = '';
-	let searchResults = [];
+	let searchResults: String[] = [];
+	let timer;
 
 	async function updateSearchResults() {
 		searchResults = await search(searchQuery);
@@ -13,23 +16,83 @@
 		console.log(event);
 		goto(`/search?query=${searchQuery}`);
 	}
+
+	const debounceSearch = async () => {
+		clearTimeout(timer);
+		if (searchQuery.length >= 3) {
+			await new Promise((resolve) => {
+				timer = setTimeout(async () => {
+					try {
+						searchResults = await search(searchQuery);
+					} catch (error) {
+						console.log(error);
+					}
+				}, 500);
+			});
+		} else {
+			searchResults = [];
+		}
+	};
+
+	function handlePageChange() {
+		searchResults = [];
+	}
+
+	function handleClickOutside(event) {
+		if (!event.target.closest('.wrapper') && !event.target.closest('.search-items')) {
+			handlePageChange();
+		}
+	}
 </script>
 
-<form class="search" on:submit={handleSubmit}>
-	<input
-		type="search"
-		placeholder="Search markdown content"
-		bind:value={searchQuery}
-		on:input={async () => {
-			updateSearchResults();
-		}}
-	/>
-	<button>Search</button>
-</form>
+<svelte:window on:click={handleClickOutside} />
+<div class="wrapper">
+	<form class="search" on:submit={handleSubmit}>
+		<input
+			type="search"
+			placeholder="Search markdown content"
+			bind:value={searchQuery}
+			on:input={async () => {
+				debounceSearch();
+			}}
+		/>
+		<button>Search</button>
+	</form>
+	{#if searchResults.length > 0}
+		<div class="search-items">
+			{#each searchResults as item, i}
+				<SearchItem data={item} on:pageChange={handlePageChange} />
+			{/each}
+		</div>
+	{/if}
+</div>
 
 <style>
+	.wrapper {
+		position: relative;
+		width: 100%;
+	}
 	.search {
 		display: flex;
 		flex: 1;
+	}
+
+	.search-items {
+		position: absolute;
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing-s);
+		top: 100%;
+		border: 1px solid var(--primary);
+		border-top: 0;
+		width: 100%;
+		left: 0;
+		z-index: 4;
+		background-color: var(--background-secondary);
+		color: var(--text-primary);
+		border-radius: var(--border-radius-s);
+		padding: var(--spacing-s);
+		transition: max-height 0.1s ease-in-out;
+		overflow-x: hidden;
 	}
 </style>
