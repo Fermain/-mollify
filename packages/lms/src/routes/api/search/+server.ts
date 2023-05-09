cafdaf085bd0800b5128e14a30ead1c675544937
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-05-09 22:56:42 +0200

    removed graphQL files

--- packages/lms/src/routes/api/search/+server.ts
+++ packages/lms/src/routes/api/search/+server.ts
@@ -1,5 +1,3 @@
-// import { graphql } from 'graphql';
-// import { schema, resolvers } from '$lib/utils/graphql/graphQLSchema';
 import { search } from '$lib/utils/fuseSearch/fuseSetup';
 
 export async function POST({ request }) {
@@ -9,17 +7,3 @@ export async function POST({ request }) {
 
 	return new Response(JSON.stringify(response));
 }
-
-// export async function POST({ request }) {
-// 	const { query, variables } = await request.json();
-
-// 	const response = await graphql({
-// 		schema,
-// 		source: query,
-// 		variableValues: variables,
-// 		contextValue: { data },
-// 		rootValue: resolvers
-// 	});
-
-// 	return new Response(JSON.stringify(response));
-// }
