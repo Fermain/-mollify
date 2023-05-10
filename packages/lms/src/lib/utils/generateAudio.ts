4ab29d2823fe79192435f8945a07c09823c0e850
Eric Pretzinger <ericpretzinger@gmail.com>
2023-05-10 15:47:57 +0200

    Getting closer, seems to write now

--- packages/lms/src/lib/utils/generateAudio.ts
+++ packages/lms/src/lib/utils/generateAudio.ts
@@ -1,7 +1,10 @@
-import { ELEVENLABS_API_KEY } from "$env/static/private";
+import { ELEVENLABS_API_KEY } from '$env/static/private';
+import fs from 'fs';
+import path from 'path';
 
 export async function generateAudio(content: string) {
 	const VOICE_ID = '21m00Tcm4TlvDq8ikWAM';
+	console.log(content);
 	try {
 		const response = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${VOICE_ID}`, {
 			method: 'POST',
@@ -10,12 +13,24 @@ export async function generateAudio(content: string) {
 				'content-type': 'application/json',
 				'xi-api-key': ELEVENLABS_API_KEY
 			},
-			body: content
+			body: JSON.stringify({
+				text: content,
+				voice_settings: {
+					stability: 0,
+					similarity_boost: 0
+				}
+			})
 		});
 
-		const { url } = await response.json();
-		console.log('15', url);
-		return url;
+		const arrayBuffer = await response.arrayBuffer();
+		const buffer = Buffer.from(arrayBuffer);
+		const file = Math.random().toString(36).substring(7);
+
+		fs.writeFile(path.join('public', 'audio', `${file}.mp3`), buffer, () => {
+			console.log('File written successfully');
+		});
+
+		return JSON.stringify({ file: `${file}.mp3` });
 	} catch (error) {
 		console.error('Error generating audio:', error);
 	}
--- packages/lms/src/routes/api/tts/+server.ts
+++ packages/lms/src/routes/api/tts/+server.ts
@@ -1,28 +1,18 @@
 import { generateAudio } from '$lib/utils/generateAudio.js';
 
 export async function POST({ request }): Promise<object> {
-	console.log(request);
 	const { text } = await request.json();
 
 	try {
 		// Convert text to audio using ElevenLabs API
 		const audio = await generateAudio(text);
-		console.log(audio);
 
-		return {
-			status: 200,
-			body: {
-				audio: audio
-			}
-		};
+		return new Response(audio);
 	} catch (error) {
 		console.error('Error converting text to speech:', error);
 
-		return {
-			status: 500,
-			body: {
-				error: 'Failed to convert text to speech'
-			}
-		};
+		return new Response(JSON.stringify({ error: 'Failed to convert text to speech' }), {
+			status: 500
+		});
 	}
 }
