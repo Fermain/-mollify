62d4bded5545d24adf76018ba88c21705f4e4382
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-05-12 00:19:43 +0200

    try catch for errors

--- packages/lms/src/lib/utils/fuseSearch/getSearchResults.ts
+++ packages/lms/src/lib/utils/fuseSearch/getSearchResults.ts
@@ -2,15 +2,19 @@ export async function getSearchResults(
 	searchQuery: string,
 	filters = { institution: 'all', programme: 'all', type: [], exact: false, exclusions: [] }
 ) {
-	const response = await fetch('/api/search', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ searchQuery, filters })
-	});
+	try {
+		const response = await fetch('/api/search', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ searchQuery, filters })
+		});
 
-	const data = await response.json();
+		const data = await response.json();
 
-	return await data;
+		return await data;
+	} catch (e) {
+		console.log(e);
+	}
 }
