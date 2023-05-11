31f9cec0bf403583579ad64f59b73114eaf031a3
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-05-12 00:21:08 +0200

    flatmap vs reduce, no difference

--- packages/lms/src/lib/utils/fuseSearch/flattenData.ts
+++ packages/lms/src/lib/utils/fuseSearch/flattenData.ts
@@ -7,19 +7,36 @@ export interface FuseItem extends Omit<EntityMeta, 'children'> {
 	children?: FuseItem[];
 }
 
-// Flatten the data so that it can be searched
+// // Flatten the data so that it can be searched
+// export function flattenData(data: EntityMeta[], parent: string | null = null) {
+// 	return data.reduce((flatData: FuseItem[], item) => {
+// 		const { children, ...itemWithoutChildren } = item;
+
+// 		// Add the current item to the flattened data
+// 		flatData.push({ ...itemWithoutChildren, parent: parent });
+
+// 		// If the item has children, recursively flatten them and add them to the flattened data
+// 		if (Array.isArray(children)) {
+// 			flatData.push(...flattenData(children, item.title));
+// 		}
+
+// 		return flatData;
+// 	}, []);
+// }
+
 export function flattenData(data: EntityMeta[], parent: string | null = null) {
-	return data.reduce((flatData: FuseItem[], item) => {
+	return data.flatMap((item) => {
 		const { children, ...itemWithoutChildren } = item;
 
-		// Add the current item to the flattened data
-		flatData.push({ ...itemWithoutChildren, parent: parent });
+		// Create a new array with the current item flattened
+		const flattenedItem = { ...itemWithoutChildren, parent: parent };
 
 		// If the item has children, recursively flatten them and add them to the flattened data
 		if (Array.isArray(children)) {
-			flatData.push(...flattenData(children, item.title));
+			const flattenedChildren = flattenData(children, item.title);
+			return [flattenedItem, ...flattenedChildren];
 		}
 
-		return flatData;
-	}, []);
+		return flattenedItem;
+	});
 }
