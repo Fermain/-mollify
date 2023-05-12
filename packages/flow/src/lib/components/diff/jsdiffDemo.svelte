b422303c02023a36e8ae4cfcf49ff4a77118bc32
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-05-12 06:20:55 +0200

    Adds previous alternative

--- packages/flow/src/lib/components/diff/jsdiffDemo.svelte
+++ packages/flow/src/lib/components/diff/jsdiffDemo.svelte
@@ -1,3 +1,33 @@
+<!-- <script lang="ts">
+	import * as Diff from 'diff';
+
+	const one =
+		'Owls are nocturnal birds of prey known for their exceptional night vision and silent flight.They are found on every continent except Antarctica and are characterized by their large, forward-facing eyes, flat faces, and powerful talons. Owls belong to the order Strigiformes and are divided into two distinct families: Tytonidae (barn owls) and Strigidae (true owls).';
+	const other =
+		'Owls are nocturnal birds of prey known for their exceptional night vision and silent flight. They inhabit every continent except Antarctica and are characterized by their large, forward-facing eyes, flat faces, and powerful talons. Owls belong to the order Strigiformes and are divided into two distinct families: Tytonidae (barn owls) and Strigidae (true owls).';
+
+	const diff = Diff.diffWordsWithSpace(one, other);
+
+	export let test = diff
+		.map((part) => {
+			const color = part.added ? '#97f295' : part.removed ? '#ffb6ba' : '#eee';
+			const chunk = `<span style="background-color: ${color};">${part.value}</span>`;
+			return chunk;
+		})
+		.join('');
+</script>
+
+<div class="demo pull-request">
+	{@html test}
+</div>
+
+<style>
+	div {
+		border: 1px solid #ddd;
+		padding: 2rem 1rem;
+	}
+</style> -->
+
 <script lang="ts">
 	import * as Diff from 'diff';
 	import { onMount } from 'svelte';
@@ -47,8 +77,6 @@
 			options
 		);
 	});
-
-	console.log(output);
 </script>
 
 {#if output}
@@ -57,11 +85,6 @@
 			<h3>File name:</h3>
 			<span class="old">{output.oldFileName}</span>
 			<span>{output.newFileName}</span>
-		</div>
-
-		<div class="diff-wrapper">
-			<h3>{output.oldHeader}</h3>
-			<h3>{output.newHeader}</h3>
 		</div>
 
 		<div>
@@ -84,7 +107,7 @@
 {/if}
 
 <style>
-	div {
+	.demo {
 		border: 1px solid #ddd;
 		padding: 2rem 1rem;
 	}
