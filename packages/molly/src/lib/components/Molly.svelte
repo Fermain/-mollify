c82c3bb836a1cab05fb4123458d393c48d7471d9
Jenny Feragen <jenny@feragen.no>
2023-05-16 15:10:45 +0200

    moving form out of main comp and deleting unused comp

--- packages/molly/src/lib/components/Molly.svelte
+++ packages/molly/src/lib/components/Molly.svelte
@@ -4,21 +4,13 @@
 	import { SSE } from 'sse.js';
 	import MollyButton from './MollyButton.svelte';
 	import MollyWindow from './MollyWindow.svelte';
-	import sendChat from './icons/paper-plane.svg';
-	export let img: string = sendChat;
+	import MollyForm from './MollyForm.svelte';
 
 	let query: string = '';
 	let answer: string = '';
 	let loading: boolean = false;
 	let chatMessages: ChatCompletionRequestMessage[] = [];
-	let scrollToDiv: HTMLDivElement;
 	export let endpoint = '/';
-
-	function scrollToBottom() {
-		setTimeout(function () {
-			scrollToDiv.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-		}, 100);
-	}
 
 	const handleSubmit = async () => {
 		loading = true;
@@ -31,12 +23,9 @@
 			payload: JSON.stringify({ messages: chatMessages })
 		});
 
-		query = '';
-
 		eventSource.addEventListener('error', handleError);
 
 		eventSource.addEventListener('message', (e) => {
-			scrollToBottom();
 			try {
 				loading = false;
 				if (e.data === '[DONE]') {
@@ -56,7 +45,6 @@
 			}
 		});
 		eventSource.stream();
-		scrollToBottom();
 	};
 
 	function handleError<T>(err: T) {
@@ -80,18 +68,12 @@
 					<MollyMessage type="assistant" message="Thinking.." />
 				{/if}
 			</div>
-			<div bind:this={scrollToDiv} />
-		<form on:submit|preventDefault={() => handleSubmit()}>
-			<textarea bind:value={query} />
-			<button type="submit"> <img src={img} alt="Send message"> </button>
-		</form>
-		
-		<!--<MollyForm
+		<MollyForm
 			on:userSubmit={(e) => {
 				query = e.detail;
 				handleSubmit();
 			}}
-		/>-->
+		/>
 		</MollyWindow>
 </MollyButton>
 
@@ -100,40 +82,12 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: flex-end;
-		height: 100%;
+		height: 300%;
 		overflow-y: scroll; /*scrolls the content if it overflows the viewport IT'S NOT WORKING*/
 	}
 
-	form {
-		background-color: #323d47;
-		padding: 10px;
-		display: flex;
-		align-items: center;
-		justify-content: space-evenly;
-		bottom: 0;
-	}
-
-	textarea {
-		max-width: 70%;
-		min-width: 70%;
-		max-height: 100px;
-		border-radius: 5px;
-		padding: 5px;
-		resize: none; /*prevents the user from manually resizing the field*/
-	}
-
-	button {
-		padding: 10px;
-		width: 50px;
-		border: none;
-		border-radius: 10px;
-		background-color: #21a299;
-		color: white;
-		cursor: pointer;
-	}
-
-	img {
-		width: 20px;
-		height: 20px;
+	.messages-container::-webkit-scrollbar {
+		width: 0.5rem;
+		color: #21a299;
 	}
 </style>
