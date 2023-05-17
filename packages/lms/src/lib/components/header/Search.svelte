ce895c1a5d15402a3f953eddb2ed7ec8b1b23d13
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-05-17 20:19:04 +0200

    Implements skeleton UI

--- packages/lms/src/lib/components/header/Search.svelte
+++ packages/lms/src/lib/components/header/Search.svelte
@@ -42,23 +42,29 @@
 </script>
 
 <svelte:window on:click={handleClickOutside} />
-<div class="wrapper">
-	<form class="search" on:submit={handleSubmit}>
+<div class="relative">
+	<form class="flex" on:submit={handleSubmit}>
 		<input
+			class="focus:outline-none focus:ring focus:ring-primary-300/50 hidden sm:block rounded-sm w-60 pl-1"
 			type="search"
+			name="autocomplete-search"
 			placeholder="Search markdown content"
 			bind:value={searchQuery}
 			on:input={async () => {
 				debounceSearch();
 			}}
 		/>
-		<button>Search</button>
+		<button class="btn hover:bg-primary-hover-token"
+			><span class="material-symbols-outlined"> search </span></button
+		>
 	</form>
-	{#if searchResults.length > 0}
-		<div class="search-items">
-			{#each searchResults as item, i}
-				<SearchItem data={item} on:pageChange={handlePageChange} />
-			{/each}
-		</div>
-	{/if}
+	<div class="absolute">
+		{#if searchResults.length > 0}
+			<div class="card p-4 w-72 shadow-xl">
+				{#each searchResults as item, i}
+					<SearchItem data={item} on:pageChange={handlePageChange} />
+				{/each}
+			</div>
+		{/if}
+	</div>
 </div>
