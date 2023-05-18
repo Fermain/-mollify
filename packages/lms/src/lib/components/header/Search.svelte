85e98639e9f2634413bd58f80656b6dcc5687408
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-05-18 09:56:19 +0200

    on blur/focus added, no result message added

--- packages/lms/src/lib/components/header/Search.svelte
+++ packages/lms/src/lib/components/header/Search.svelte
@@ -7,6 +7,7 @@
 	let searchQuery = '';
 	let searchResults: String[] = [];
 	let timer: string | number | NodeJS.Timeout | undefined;
+	let inputFocused = false;
 
 	function handleSubmit(event: { preventDefault: () => void }) {
 		event.preventDefault();
@@ -54,14 +55,25 @@
 			on:input={async () => {
 				debounceSearch();
 			}}
+			on:focus={() => {
+				inputFocused = true;
+			}}
+			on:blur={() => {
+				inputFocused = false;
+			}}
 		/>
 		<button>Search</button>
 	</form>
-	{#if searchResults.length > 0}
+	{#if searchResults.length > 0 && inputFocused}
 		<div class="search-items">
 			{#each searchResults as item, i}
 				<SearchItem data={item} on:pageChange={handlePageChange} />
 			{/each}
+		</div>
+	{/if}
+	{#if searchResults.length === 0 && inputFocused && searchQuery.length >= 3}
+		<div class="search-items">
+			<p class="no-results">No Results Found</p>
 		</div>
 	{/if}
 </div>
@@ -89,5 +101,11 @@
 		padding: var(--spacing-s);
 		transition: max-height 0.1s ease-in-out;
 		overflow-x: hidden;
+	}
+
+	.no-results {
+		padding: var(--spacing-s) var(--spacing-xxs);
+		background-color: var(--background-primary);
+		border-radius: var(--spacing-xxs);
 	}
 </style>
