992b0cd6f149d694ca7f82f057ab50364f0a280b
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-05-18 14:12:12 +0200

    Implements skeleton UI on search form

--- packages/lms/src/routes/search/+page.svelte
+++ packages/lms/src/routes/search/+page.svelte
@@ -42,16 +42,36 @@
 	$: $files;
 </script>
 
-<section>
-	<h1>Search</h1>
+<section class="p-4">
+	<h1 class="h1 mb-8">Search</h1>
 	<div class="search-wrapper">
 		<form class="search" on:submit={handleSubmit}>
-			<div>
-				<label for="search">Search Query</label>
+			<label class="label flex flex-col" for="search">
+				<span class="font-medium">Search query</span>
+				<input
+					class="input sm:w-2/4"
+					type="search"
+					id="search"
+					placeholder="Search markdown content"
+					bind:value={searchQuery}
+				/>
+			</label>
+			<label class="flex items-center space-x-2 mt-1 mb-6" for="search-exact">
+				<input
+					class="checkbox"
+					type="checkbox"
+					value={true}
+					id="search-exact"
+					bind:checked={searchQueryExact}
+				/>
+				<p>Exact Match</p>
+			</label>
+			<!-- <div>
+				<label for="search" class="label">Search Query</label>
 				<input type="search" placeholder="Search markdown content" bind:value={searchQuery} />
-			</div>
+			</div> -->
 			<div class="search-options">
-				<div>
+				<!-- <div>
 					<label for="search-exact">Exact Match</label>
 					<input
 						type="checkbox"
@@ -60,9 +80,9 @@
 						id="search-exact"
 						bind:checked={searchQueryExact}
 					/>
-				</div>
+				</div> -->
 				{#if $files?.length > 1}
-					<div>
+					<!-- <div>
 						<label>Institution</label>
 						<select bind:value={selectedInstitution}>
 							<option value="all">All</option>
@@ -70,54 +90,113 @@
 								<option value={file.foldername}>{file.title}</option>
 							{/each}
 						</select>
-					</div>
+					</div> -->
+					<label class="label font-medium" for="institution-options">Institution</label>
+					<select
+						class="select mt-1 rounded-md sm:w-2/4"
+						bind:value={selectedInstitution}
+						id="institution-options"
+					>
+						<option value="all">All</option>
+						{#each $files as file}
+							<option value={file.foldername}>{file.title}</option>
+						{/each}
+					</select>
 				{/if}
-				<div>
-					<fieldset>
-						<legend>Search Type</legend>
-						<label for="programme">Programmes</label>
-						<input
-							type="checkbox"
-							name="type"
-							value="programme"
-							id="programme"
-							bind:group={searchTypes}
-						/>
-						<label for="course">Courses</label>
+				<div class="my-6">
+					<span class="label font-medium">Search type</span>
+					<div class="flex gap-4 flex-wrap">
+						<!-- <fieldset>
+						<legend>Search Type</legend> -->
+						<label class="flex items-center space-x-2 mt-1" for="programme">
+							<input
+								class="checkbox"
+								type="checkbox"
+								name="type"
+								value="programme"
+								id="programme"
+								bind:group={searchTypes}
+							/>
+							<p>Programmes</p>
+						</label>
+
+						<label class="flex items-center space-x-2 mt-1" for="course">
+							<input
+								class="checkbox"
+								type="checkbox"
+								name="type"
+								value="course"
+								id="course"
+								bind:group={searchTypes}
+							/>
+							<p>Courses</p>
+						</label>
+
+						<label class="flex items-center space-x-2 mt-1" for="module">
+							<input
+								class="checkbox"
+								type="checkbox"
+								name="type"
+								value="module"
+								id="module"
+								bind:group={searchTypes}
+							/>
+							<p>Modules</p>
+						</label>
+
+						<label class="flex items-center space-x-2 mt-1" for="lesson">
+							<input
+								class="checkbox"
+								type="checkbox"
+								name="type"
+								value="lesson"
+								id="lesson"
+								bind:group={searchTypes}
+							/>
+							<p>Lessons</p>
+						</label>
+
+						<!-- <label for="course">Courses</label>
 						<input
 							type="checkbox"
 							name="type"
 							value="course"
 							id="course"
 							bind:group={searchTypes}
-						/>
-						<label for="module">Modules</label>
+						/> -->
+						<!-- <label for="module">Modules</label>
 						<input
 							type="checkbox"
 							name="type"
 							value="module"
 							id="module"
 							bind:group={searchTypes}
-						/>
-						<label for="lesson">Lessons</label>
+						/> -->
+						<!-- <label for="lesson">Lessons</label>
 						<input
 							type="checkbox"
 							name="type"
 							value="lesson"
 							id="lesson"
 							bind:group={searchTypes}
-						/>
-					</fieldset>
+						/> -->
+						<!-- </fieldset> -->
+					</div>
 				</div>
 			</div>
-			<div>
-				<button class="show-option-btn" type="button">Advanced Search Options</button>
+			<div class="flex flex-wrap my-8 gap-4">
+				<button type="submit" class="btn variant-filled-primary">Search</button>
+				<div>
+					<button class="btn variant-outline-primary variant-filled-secondary" type="button"
+						>Advanced Search Options</button
+					>
+				</div>
 			</div>
-			<button type="submit" class="search-btn">Search</button>
 		</form>
 	</div>
 	<div class="results-container">
-		{#if searchResults}
+		{#if searchResults.length > 0}
+			<h2 class="h2">Search results</h2>
 			{#each searchResults as result}
 				<div class="result">
 					<h3>{result.title}</h3>
@@ -130,7 +209,7 @@
 	</div>
 </section>
 
-<style lang="scss">
+<!-- <style lang="scss">
 	main {
 		padding-top: var(--spacing-xl);
 		min-height: 100vh;
@@ -188,4 +267,4 @@
 		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
 		grid-gap: 1rem;
 	}
-</style>
+</style> -->
