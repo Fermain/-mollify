0e57bb37ec538e4a4a6a690cbb9db967441e206c
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-05-18 14:49:23 +0200

    Implements skeleton UI on search results

--- packages/lms/src/routes/search/+page.svelte
+++ packages/lms/src/routes/search/+page.svelte
@@ -66,31 +66,8 @@
 				/>
 				<p>Exact Match</p>
 			</label>
-			<!-- <div>
-				<label for="search" class="label">Search Query</label>
-				<input type="search" placeholder="Search markdown content" bind:value={searchQuery} />
-			</div> -->
 			<div class="search-options">
-				<!-- <div>
-					<label for="search-exact">Exact Match</label>
-					<input
-						type="checkbox"
-						placeholder="Search markdown content"
-						value={true}
-						id="search-exact"
-						bind:checked={searchQueryExact}
-					/>
-				</div> -->
 				{#if $files?.length > 1}
-					<!-- <div>
-						<label>Institution</label>
-						<select bind:value={selectedInstitution}>
-							<option value="all">All</option>
-							{#each $files as file}
-								<option value={file.foldername}>{file.title}</option>
-							{/each}
-						</select>
-					</div> -->
 					<label class="label font-medium" for="institution-options">Institution</label>
 					<select
 						class="select mt-1 rounded-md sm:w-2/4"
@@ -106,8 +83,6 @@
 				<div class="my-6">
 					<span class="label font-medium">Search type</span>
 					<div class="flex gap-4 flex-wrap">
-						<!-- <fieldset>
-						<legend>Search Type</legend> -->
 						<label class="flex items-center space-x-2 mt-1" for="programme">
 							<input
 								class="checkbox"
@@ -155,32 +130,6 @@
 							/>
 							<p>Lessons</p>
 						</label>
-
-						<!-- <label for="course">Courses</label>
-						<input
-							type="checkbox"
-							name="type"
-							value="course"
-							id="course"
-							bind:group={searchTypes}
-						/> -->
-						<!-- <label for="module">Modules</label>
-						<input
-							type="checkbox"
-							name="type"
-							value="module"
-							id="module"
-							bind:group={searchTypes}
-						/> -->
-						<!-- <label for="lesson">Lessons</label>
-						<input
-							type="checkbox"
-							name="type"
-							value="lesson"
-							id="lesson"
-							bind:group={searchTypes}
-						/> -->
-						<!-- </fieldset> -->
 					</div>
 				</div>
 			</div>
@@ -196,75 +145,23 @@
 	</div>
 	<div class="results-container">
 		{#if searchResults.length > 0}
-			<h2 class="h2">Search results</h2>
-			{#each searchResults as result}
-				<div class="result">
-					<h3>{result.title}</h3>
-					<p>Search Score:{Math.round(result.score * 10000) / 10000}</p>
-					<p>Type: {result.type}</p>
-					<a class="result-btn" href={result.browserPath}>View Details</a>
-				</div>
-			{/each}
+			<h2 class="h2 mb-8">Search results</h2>
+			<div class="grid sm:grid-cols-2 gap-4">
+				{#each searchResults as result}
+					<a class="card p-4 variant-ghost-surface" href={result.browserPath}>
+						<header class="card-header border-b pb-2"><h3 class="h3">{result.title}</h3></header>
+						<section class="p-4">
+							<p>Search Score: {Math.round(result.score * 10000) / 10000}</p>
+							<p>Type: {result.type}</p>
+						</section>
+						<footer class="card-footer border-t pt-2">
+							<span class="chip variant-filled">Cool</span>
+							<span class="chip variant-filled">Noice</span>
+							<span class="chip variant-filled">Give me more</span>
+						</footer>
+					</a>
+				{/each}
+			</div>
 		{/if}
 	</div>
 </section>
-
-<!-- <style lang="scss">
-	main {
-		padding-top: var(--spacing-xl);
-		min-height: 100vh;
-		grid-area: main;
-	}
-
-	.search-wrapper {
-		display: flex;
-		flex-direction: column;
-		padding: var(--spacing-m);
-	}
-	.result-btn,
-	.search-btn {
-		background-color: var(--secondary);
-		color: var(--text-primary);
-		margin: auto;
-
-		:hover {
-			background-color: var(--text-primary);
-			color: var(--secondary);
-		}
-	}
-
-	.result-btn {
-		display: block;
-		padding: var(--spacing-s) var(--spacing-m);
-		border-radius: var(--spacing-s);
-		align-self: center;
-		margin: auto;
-		width: fit-content;
-	}
-
-	.show-option-btn {
-		border: none;
-		background-color: transparent;
-		color: var(--text-primary);
-		text-decoration: underline;
-		margin-left: auto;
-	}
-	.search > div,
-	.search-options > div {
-		margin-bottom: var(--spacing-m);
-	}
-
-	.result {
-		background-color: var(--primary);
-		color: var(--text-secondary);
-		padding: var(--spacing-m);
-		border-radius: var(--spacing-m);
-	}
-
-	.results-container {
-		padding: 0.5rem;
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-		grid-gap: 1rem;
-	}
-</style> -->
