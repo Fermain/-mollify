8b1dea572d11606f1284cc21d91db62c6c58443d
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-05-21 08:28:03 +0200

    Installs and compares text sentence by sentence

--- packages/flow/src/lib/components/InkMDEDemo.svelte
+++ packages/flow/src/lib/components/InkMDEDemo.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { onMount, afterUpdate } from 'svelte';
 	import InkMde from 'ink-mde/svelte';
 	import diff from 'simple-text-diff';
@@ -112,5 +112,111 @@
 		border: 1px solid #eee;
 		padding: 0.5rem;
 		flex: 0 0 45%;
+	}
+</style> -->
+
+<script lang="ts">
+	import * as Diff from 'diff';
+	import InkMde from 'ink-mde/svelte';
+	import { onMount, afterUpdate } from 'svelte';
+	import { marked } from 'marked';
+
+	let apiCall: string = '/api/fetchRemote';
+	let oldText: string = '';
+	let value: string = '';
+
+	let differences: {
+		added?: boolean;
+		removed?: boolean;
+		value: string;
+	}[] = [];
+
+	const options = {
+		context: 10,
+		ignoreWhiteSpace: true,
+		newlineIsToken: true
+	};
+
+	onMount(async () => {
+		const response = await fetch(apiCall);
+		const data: string = await response.text();
+		oldText = data;
+		value = data;
+	});
+
+	function displayContents(): void {
+		const diffResult = Diff.diffSentences(oldText, value, options);
+		differences = diffResult;
+
+		console.log(diffResult);
+	}
+
+	function formatMarkdown(text: string): string {
+		return marked(text);
+	}
+</script>
+
+<div>
+	<h1>Editor</h1>
+	<div class="btn-wrapper">
+		<a href="/#demo" class="primary-btn" on:click={displayContents}>Compare changes</a>
+	</div>
+	<InkMde
+		bind:value
+		options={{
+			interface: {
+				appearance: 'light',
+				toolbar: true
+			},
+			readability: true,
+			toolbar: {
+				bold: true,
+				code: true,
+				codeBlock: true,
+				heading: true,
+				image: true,
+				italic: true,
+				link: true,
+				list: true,
+				orderedList: true,
+				quote: true,
+				taskList: true,
+				upload: true
+			}
+		}}
+	/>
+	<div class="btn-wrapper">
+		<a href="/#demo" class="primary-btn" on:click={displayContents}>Compare changes</a>
+	</div>
+	{#if differences.length > 0}
+		<div class="demo pull-request">
+			{#each differences as diff (diff.value)}
+				{#if diff.added}
+					<div>
+						<p class="added">{@html formatMarkdown(diff.value)}</p>
+					</div>
+				{:else if diff.removed}
+					<div>
+						<p class="removed">{@html formatMarkdown(diff.value)}</p>
+					</div>
+				{:else}
+					<p>{@html formatMarkdown(diff.value)}</p>
+				{/if}
+			{/each}
+		</div>
+	{/if}
+</div>
+
+<style>
+	.demo {
+		border: 1px solid #ddd;
+		padding: 2rem 1rem;
+		display: flex;
+	}
+	.added {
+		color: green;
+	}
+	.removed {
+		color: red;
 	}
 </style>
