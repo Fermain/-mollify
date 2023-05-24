ac03a0c7a81d4ffde3fe9b516666d24f2dc280f3
Jenny Feragen <jenny@feragen.no>
2023-05-24 09:34:13 +0200

    config file in molly

--- packages/molly/tailwind.config.cjs
+++ packages/molly/tailwind.config.cjs
@@ -1,14 +1,15 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	// 1. Apply the dark mode class setting:
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		// 2. Append the path for the Skeleton NPM package and files:
 		require('path').join(require.resolve(
 			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
+			'../**/*.{html,js,svelte,ts}',
+			"./pages/**/*.{html,js,ts,svelte}",
+			"./components/**/*.{html,js,ts,svelte}",
+			"../../packages/ui/**/*.{html,js,ts,svelte}"
 		)
 	],
 	theme: {
