722413dcc6e1d876b087ccd628e45a62121fc1e3
Fermain <Fermain@users.noreply.github.com>
2023-05-24 07:57:50 +0200

    Adds unlink command

--- packages/cli/src/actions/symlink/ensure.ts
+++ packages/cli/src/actions/symlink/ensure.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs-extra';
 import { PACKAGE_INVENTORY, SYMLINK_DIR } from '../../constants';
+import { log } from 'console';
 const [LMS] = PACKAGE_INVENTORY;
 
 export async function ensureSymlink(
@@ -8,8 +9,10 @@ export async function ensureSymlink(
   destination = path.join(process.cwd(), 'node_modules', LMS, 'src/routes'),
 ) {
   try {
+    log(`Ensuring symlink from ${origin} to ${destination}`)
     await fs.ensureSymlink(origin, destination);
   } catch (error: any) {
+    log(`Error ensuring symlink: ${error}`);
     if (error.code !== 'EEXIST') {
       throw error;
     }
--- packages/cli/src/commands/unlink/index.ts
+++ packages/cli/src/commands/unlink/index.ts
@@ -1,26 +1,41 @@
-import { Command } from 'commander';
-import actions from '../../actions';
+import git from '../../actions/git';
+import symlink from '../../actions/symlink';
+import yargs from 'yargs';
 
-export default new Command('unlink')
-  .arguments('<mode> [destination]')
-  .description(
-    'Unlink a remote or local entity that was previously linked as a submodule or symlink.',
-  )
-  .action(async (mode, destination) => {
+export default {
+  command: 'unlink <mode> <destination>',
+  desc: 'Unlink a remote or local entity that was previously linked as a submodule or symlink.',
+  builder: (yargs: yargs.Argv) => {
+    return yargs
+      .positional('mode', {
+        describe: 'Mode for the unlinking operation (remote or local)',
+        type: 'string',
+        demandOption: true,
+      })
+      .positional('destination', {
+        describe: 'Location in this project where the entity is currently linked',
+        type: 'string',
+        default: process.cwd(),
+        demandOption: true,
+      });
+  },
+  handler: async (argv: any) => {
+    const mode = argv.mode;
+    const destination = argv.destination;
+
     try {
       switch (mode) {
         case 'remote':
-          await actions.git.submodule.unregister(destination);
+          await git.submodule.unregister(destination);
           break;
         case 'local':
-          await actions.symlink.remove(destination);
+          await symlink.remove(destination);
           break;
         default:
-          console.error(
-            `Invalid mode: ${mode}. Supported modes are 'remote' and 'local'.`,
-          );
+          throw new Error(`Invalid mode: ${mode}. Supported modes are 'remote' and 'local'.`);
       }
     } catch (error) {
       console.error(`Error: ${error}`);
     }
-  });
+  },
+};
