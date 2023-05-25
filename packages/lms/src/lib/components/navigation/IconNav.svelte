9a9be6c06ef4ff2c6ec0ddf27538bedc5885c095
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-05-25 15:58:44 +0200

    added button to apprail

--- packages/lms/src/lib/components/navigation/IconNav.svelte
+++ packages/lms/src/lib/components/navigation/IconNav.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { AppRail, AppRailTile } from '@skeletonlabs/skeleton';
+	import AddRemoveBookmarks from './icons/AddRemoveBookmarks.svelte';
 </script>
 
 <AppRail width="w-[3rem] sm:w-20">
-	<AppRailTile value={0}><i class="icon-f">home</i></AppRailTile>
+	<AppRailTile name="tile-1" value={0}><i class="icon-f">home </i></AppRailTile>
 	<AppRailTile value={1}><i class="icon-f">bookmark</i></AppRailTile>
-	<AppRailTile value={2}><i class="icon-f">bookmark_add</i></AppRailTile>
+	<AddRemoveBookmarks />
 </AppRail>
