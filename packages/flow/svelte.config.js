6fe4f5fdceab08256cd73ee18128c96949f268af
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-05-25 17:41:51 +0200

    Installs and configures callouts plugin

--- packages/flow/svelte.config.js
+++ packages/flow/svelte.config.js
@@ -1,11 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import callouts from 'remark-emoji-callout';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			highlight: {},
+			extensions: ['.md'],
+			remarkPlugins: [callouts]
+		})
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
