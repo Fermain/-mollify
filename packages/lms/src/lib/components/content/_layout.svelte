09085e25b5f5726756a11810c2d8fc2fd65d5656
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-05-26 17:48:22 +0200

    tt

--- packages/lms/src/lib/components/content/_layout.svelte
+++ packages/lms/src/lib/components/content/_layout.svelte
@@ -1,4 +1,11 @@
 <script>
+	import { onMount } from 'svelte';
+	export let title;
+	export let tags;
+	onMount(() => {
+		console.log('title', title);
+		console.log('tags', tags);
+	});
 </script>
 
 <slot />
