6a0fd8079aa9998c789ccae7b1fb49befbea0fa9
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-05-26 17:14:46 +0200

    added onclick function to window

--- packages/lms/src/lib/components/content/Main.svelte
+++ packages/lms/src/lib/components/content/Main.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import ProgrammeNav from '../navigation/ProgrammeNav.svelte';
+	import { browser } from '$app/environment';
+	import { AddRemoveBookmarks } from '$lib/utils/bookmarking/AddRemove';
+	import { bookmarks } from '$lib/stores/bookmarks';
+	let bookmarksStore = bookmarks;
+
+	if (browser) {
+		window.addBookmark = async function addBookmark(id) {
+			console.log('addBookmark', id);
+			bookmarksStore = await AddRemoveBookmarks(id);
+			bookmarks.set(bookmarksStore);
+		};
+	}
 </script>
 
 <main class="p-5 w-full">
