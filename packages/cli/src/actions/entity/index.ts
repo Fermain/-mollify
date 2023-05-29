2a6f10dd9fc1675855a43f16abf4f99b54ce358c
Fermain <Fermain@users.noreply.github.com>
2023-05-29 11:25:29 +0200

    Moves slug functionality to CLI

--- packages/cli/src/actions/entity/index.ts
+++ packages/cli/src/actions/entity/index.ts
@@ -2,10 +2,12 @@ import { createEntity } from './create';
 import { moveEntity } from './move';
 import { removeEntity } from './remove';
 import { listEntities } from './list';
+import slug from './slug';
 
 export default {
   create: createEntity,
   move: moveEntity,
   remove: removeEntity,
-  list: listEntities
+  list: listEntities,
+  slug
 };
