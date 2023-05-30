def131446266309b689fefeeb85a5f6c2f344b40
Fermain <Fermain@users.noreply.github.com>
2023-05-30 07:24:11 +0200

    Simplifies types

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,1 +1,32 @@
-export * from "./lib";
+// Enumeration of entity types
+export enum EntityType {
+    Institution = "Institution",
+    Programme = "Programme",
+    Course = "Course",
+    Module = "Module",
+    Lesson = "Lesson",
+    Assessment = "Assessment",
+}
+
+// Base interface for entity metadata
+// This is the fundamental information contained in frontmatter
+export interface EntityBase {
+    type: EntityType;
+    title: string;
+    url?: string;
+    summary?: string;
+    tags: Array<string>;
+    previous?: string;
+    content?: string;
+    [key: string]: unknown;
+}
+
+// Extended interface for entity metadata, including slug, children, and address
+// These represent dynamic values that must come from disk
+export interface EntityMeta extends EntityBase {
+    slug: string;
+    browserPath?: string;
+    children: Array<EntityMeta>;
+    address: string;
+}
+
