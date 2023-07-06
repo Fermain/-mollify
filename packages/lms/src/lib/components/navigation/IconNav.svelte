2892e943df2aa613ba5f2761aa502cecaedd1412
Oliver Dipple <Fermain@users.noreply.github.com>
2023-07-06 12:05:06 +0200

    Update packages/lms/src/lib/components/navigation/IconNav.svelte
    Co-authored-by: Eric Pretzinger <ericpretzinger@gmail.com>

--- packages/lms/src/lib/components/navigation/IconNav.svelte
+++ packages/lms/src/lib/components/navigation/IconNav.svelte
@@ -5,7 +5,6 @@
 </script>
 
 <AppRail width="w-[3rem] sm:w-20">
-	<!-- <AppRailTile name="tile-1" value={0}><i class="icon-f">home </i></AppRailTile> -->
 	<Bookmarks />
 	<AddRemoveBookmarks />
 </AppRail>
