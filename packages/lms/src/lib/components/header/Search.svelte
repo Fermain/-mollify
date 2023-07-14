8b10e7f512514c122da6dd281cbfd785e1383020
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-07-14 11:51:22 -0500

    Adds keyboard navigation functionality

--- packages/lms/src/lib/components/header/Search.svelte
+++ packages/lms/src/lib/components/header/Search.svelte
@@ -1,19 +1,34 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { getSearchResults } from '$lib/utils/fuseSearch/getSearchResults';
-	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
 	import { parseRawSearchQuery } from '$lib/utils/fuseSearch/parseRawSearchQuery';
 
 	let searchQuery = '';
-	let searchResults: String[] = [];
+	let searchResults: { label: string; value: string; refIndex: number; browserPath: string }[] = [];
 	let timer: string | number | NodeJS.Timeout | undefined;
 	let inputFocused = false;
 	let returnedResults = false;
 	let rawQuery = '';
 
+	//these handle keyboard navigation
+	//set to -1 so that the user can select the first search suggestion which in an array would be 0
+	let selection: number = -1;
+	let pathToNavigateTo: string = '';
+
+	interface SearchResultItem {
+		title: string;
+		slug: string;
+		refIndex: number;
+	}
+
 	function handleSubmit(event: { preventDefault: () => void }) {
-		goto(`/search?query=${encodeURIComponent(searchQuery)}`);
+		if (pathToNavigateTo === '') {
+			goto(`/search?query=${encodeURIComponent(searchQuery)}`);
+		} else {
+			goto(pathToNavigateTo);
+		}
 		searchQuery = '';
+		selection = -1;
 	}
 
 	const debounceSearch = async () => {
@@ -26,7 +41,7 @@
 						const { query, filters } = parseRawSearchQuery(searchQuery);
 						rawQuery = query;
 						let results = await getSearchResults(query, filters);
-						searchResults = results.map((result: AutocompleteOption[]) => {
+						searchResults = results.map((result: SearchResultItem) => {
 							const { title, slug, ...other } = result;
 							return { label: title, value: slug, ...other };
 						});
@@ -42,22 +57,18 @@
 		}
 	};
 
-	//my function
-
-	let selected = -1;
-
-	function handleKeyboardNavigation(event): void {
+	function handleKeyboardNavigation(event: KeyboardEvent): void {
 		const key = event.key;
 
 		switch (key) {
-			case 'Enter':
-				// goToPage();
-				break;
 			case 'ArrowDown':
 				navigateSearchResults(1);
 				break;
 			case 'ArrowUp':
 				navigateSearchResults(-1);
+				break;
+			case 'Backspace' || 'Delete':
+				selection = -1;
 				break;
 			default:
 				break;
@@ -65,17 +76,40 @@
 	}
 
 	function navigateSearchResults(direction: number): void {
-		const newSelected = selected + direction;
-		if (newSelected >= 0 && newSelected < searchResults.length) {
-			selected = newSelected;
+		//selects the search result
+		const newSelection = selection + direction;
+		if (newSelection >= 0 && newSelection < searchResults.length) {
+			selection = newSelection;
 		}
-
-		const selectedSuggestion = searchResults[selected].refIndex;
+		//styles the selected search result
+		const selectedSuggestion = searchResults[selection].refIndex;
 		searchResults.forEach((result) => {
-			const domElement = document.getElementById(result.refIndex);
+			const domElement = document.getElementById(result.refIndex.toString());
 			result.refIndex === selectedSuggestion
 				? domElement?.classList.add('bg-primary-300/25')
 				: domElement?.classList.remove('bg-primary-300/25');
+			//scrolls down if selected element isn't in view
+			if (domElement && domElement?.offsetTop > 140) {
+				result.refIndex === selectedSuggestion &&
+					domElement?.scrollIntoView({
+						behavior: 'smooth',
+						block: 'nearest',
+						inline: 'start'
+					});
+			}
+			//scrolls up if selected element isn't in view
+			if (domElement && domElement?.offsetTop < 140) {
+				result.refIndex === selectedSuggestion &&
+					domElement?.scrollIntoView({
+						behavior: 'smooth',
+						block: 'nearest',
+						inline: 'start'
+					});
+			}
+
+			if (result.refIndex === selectedSuggestion) {
+				pathToNavigateTo = result.browserPath;
+			}
 		});
 	}
 
@@ -83,16 +117,19 @@
 		setTimeout(() => {
 			searchResults = [];
 			searchQuery = '';
+			selection = -1;
 		}, 10);
 	}
 
-	function handleClickOutside(event: object) {
-		if (!event.target.closest('.wrapper') && !event.target.closest('.search-items')) {
+	function handleClickOutside(event: MouseEvent | TouchEvent) {
+		const target = event.target as Element;
+
+		if (!target.closest('.wrapper') && !target.closest('.search-items')) {
 			handlePageChange();
 		}
 	}
 
-	function handleSearchSelection(path) {
+	function handleSearchSelection(path: string) {
 		goto(path);
 	}
 
@@ -132,11 +169,12 @@
 		<dl class="list-dl w-full max-h-48 p-4 overflow-y-auto absolute bg-surface-100-800-token">
 			{#each searchResults as item, i}
 				<div
-					id={item.refIndex}
+					id={item.refIndex.toString()}
 					class="hover:bg-primary-hover-token rounded-container-token cursor-pointer"
 					on:click={() => {
 						handleSearchSelection(item.browserPath);
 					}}
+					on:keydown={handleKeyboardNavigation}
 				>
 					<span class="flex-auto w-full fill-current transition-transform duration-[200ms]">
 						<dt class="truncate">{item.label}</dt>
