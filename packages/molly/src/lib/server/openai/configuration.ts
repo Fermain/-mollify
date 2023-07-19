e9ef8f391271b8b95e6abcd07aceefe4405d01df
Fermain <Fermain@users.noreply.github.com>
2023-07-19 11:33:42 +0200

    Adds optional config pattern to molly

--- packages/molly/src/lib/server/openai/configuration.ts
+++ packages/molly/src/lib/server/openai/configuration.ts
@@ -1,3 +1,3 @@
-import { OPENAI_API_KEY as apiKey } from '$env/static/private';
+import { OPENAI_API_KEY as apiKey } from '$lib/server/optional.config';
 import { Configuration } from 'openai';
 export default new Configuration({ apiKey });
