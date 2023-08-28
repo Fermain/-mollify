4cf219553f87a564f67455306be3ad3f059cd412
Oliver Dipple <Fermain@users.noreply.github.com>
2023-08-28 09:20:51 +0200

    Update packages/lms/src/routes/+layout.server.ts
    Co-authored-by: Alexander Barrett <91462616+Anclagen@users.noreply.github.com>

--- packages/lms/src/routes/+layout.server.ts
+++ packages/lms/src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ import path from 'path';
 export const load: LayoutServerLoad = ({ url }) => {
 
   try {
-    const navPath = path.join('src/routes', path.join(...url.pathname.split('/').splice(1, 3)))
+    const navPath = path.join('src/routes', path.join(...decodeURIComponent(url.pathname).split('/').splice(1, 3)));
     const sitemap = getEntityMetaTree(navPath);
     return { sitemap };
   } catch {
