bfe550638906cd8fd14a17aa8537962959ac9c95
Lysaker-git <68820814+Lysaker-git@users.noreply.github.com>
2023-08-31 19:45:05 +0200

    A simple Improvement to right side nav
    Added variable parent to check if parent element, added icon on parent element and added more padding to child elements

--- packages/lms/src/lib/components/navigation/EntityNav.svelte
+++ packages/lms/src/lib/components/navigation/EntityNav.svelte
@@ -5,6 +5,7 @@
 	$: _entities = Array.isArray(entities) ? entities : [entities];
 
 	export let open = false;
+	export let parent = true;
 
 	function toggle() {
 		open = !open;
@@ -15,6 +16,9 @@
 	<nav class="entity">
 		<div class="entity-inner">
 			<div class="entity-header hover:bg-primary-hover-token rounded-container-token p-2">
+				{#if parent}
+					<i class="icon-f">insert_drive_file</i>
+				{/if}
 				<a href={entity.browserPath} class="entity-title">
 					{entity.title}
 				</a>
@@ -27,7 +31,7 @@
 			{#if entity.children.length}
 				<div class="entity-children" class:open>
 					{#each entity.children as child}
-						<svelte:self entities={child} />
+						<svelte:self entities={child} parent={false}/>
 					{/each}
 				</div>
 			{/if}
@@ -50,7 +54,7 @@
 		}
 
 		&-children {
-			padding-left: 0.5rem;
+			padding-left: 1.7rem;
 
 			&:not(.open) {
 				display: none;
