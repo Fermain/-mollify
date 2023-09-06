4e2063fd38dcbb028271f7b46df25f3cbb7e323a
Ethmane Didi <ousman.didibela@gmail.com>
2023-09-06 10:18:27 +0200

    Fix navigation UX in EntityNav.svelte

--- packages/lms/src/lib/components/navigation/EntityNav.svelte
+++ packages/lms/src/lib/components/navigation/EntityNav.svelte
@@ -1,31 +1,47 @@
 <script lang="ts">
 	import type { EntityMeta } from '@mollify/types';
+	import { readable, writable } from 'svelte/store';
 
 	export let entities: Array<EntityMeta> | EntityMeta = [];
 	$: _entities = Array.isArray(entities) ? entities : [entities];
 
-	export let open = false;
+	// Use Svelte writable store for the "open" state
+	let openStates = writable({}); // Initialize as a writable store
 
-	function toggle() {
-		open = !open;
+	function toggle(entityId: string) {
+		openStates.update((states) => {
+			states[entityId] = !states[entityId];
+			return { ...states };
+		});
 	}
 </script>
 
 {#each _entities as entity}
-	<nav class="entity">
+	<nav
+		class="entity"
+		tabindex="0"
+		on:keydown={(event) => {
+			if (event.key === 'Enter' || event.key === 'Space') {
+				toggle(entity.id);
+			}
+		}}
+	>
 		<div class="entity-inner">
-			<div class="entity-header hover:bg-primary-hover-token rounded-container-token p-2">
+			<div
+				class="entity-header hover:bg-primary-hover-token rounded-container-token p-2"
+				on:click={() => toggle(entity.id)}
+			>
 				<a href={entity.browserPath} class="entity-title">
 					{entity.title}
 				</a>
 				{#if entity.children.length}
-					<button on:click={toggle} class="btn hover:bg-primary-hover-token p-0"
-						><i class="icon-f">{open ? 'expand_less' : 'expand_more'}</i></button
-					>
+					<button class="btn hover:bg-primary-hover-token p-0">
+						<i class="icon-f">{openStates[entity.id] ? 'expand_less' : 'expand_more'}</i>
+					</button>
 				{/if}
 			</div>
 			{#if entity.children.length}
-				<div class="entity-children" class:open>
+				<div class="entity-children" class:open={$openStates[entity.id]}>
 					{#each entity.children as child}
 						<svelte:self entities={child} />
 					{/each}
@@ -41,12 +57,13 @@
 			display: flex;
 			justify-content: space-between;
 			align-items: center;
+			cursor: pointer; /* Add cursor pointer to indicate clickable element */
 		}
 
 		&-title {
 			flex: 1;
-            padding: 0.5rem 0;
-            padding-left: 0.5rem;
+			padding: 0.5rem 0;
+			padding-left: 0.5rem;
 		}
 
 		&-children {
@@ -58,3 +75,4 @@
 		}
 	}
 </style>
+
