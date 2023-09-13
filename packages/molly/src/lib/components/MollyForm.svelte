2c0fc8e73303918f9bd9cfd90e7c3325b2bab285
Megumi Kimura <kimuramegumi@gmail.com>
2023-09-13 10:38:18 +0200

    Enable enter key submit/Prevents empty query

--- packages/molly/src/lib/components/MollyForm.svelte
+++ packages/molly/src/lib/components/MollyForm.svelte
@@ -6,19 +6,34 @@
 
 	let query: string = "";
 	const dispatch = createEventDispatcher();
+
 	function handleSubmit() {
 		dispatch('userSubmit', query);
 		query = "";
 	}
+
+	function handleKeyPress(event:KeyboardEvent) {
+		//if statement to prevent empty query to be submitted
+		if(query.length === 0 && event.key === 'Enter'){
+		event.preventDefault() 
+		} else {	
+		//enable press Enter submit and Shit+Enter line break
+		if(query.length > 0 && event.key === 'Enter' && !event.shiftKey){
+			event.preventDefault()
+			handleSubmit()
+		}}
+}
+
 </script>
 
 <div class="bg-surface-100-800-token border-t border-slate-400 p-4 drop-shadow-md">
-<form on:submit|preventDefault={() => handleSubmit()} 
-	class="flex input-group sm:input-group-divider sm:grid-cols-[1fr_auto] rounded">
-	<textarea  bind:value={query} 
+	<form on:submit|preventDefault={() => handleSubmit()} 
+class="flex input-group sm:input-group-divider sm:grid-cols-[1fr_auto] rounded">
+		<textarea bind:value={query}
 		class="textarea hidden sm:block bg-slate-100 dark:bg-slate-200 rounded-none p-1 text-black"
-		placeholder="Write here..." />
-	<button type="submit" class="btn variant-filled-primary rounded-none">
-		<img src={img} alt="send message" class="h-6 w-6"/>
-	</button>
+		placeholder="Write here..."
+		on:keypress={(event) => handleKeyPress(event)} />
+		<button type="submit" class="btn variant-filled-primary rounded-none">
+			<img src={img} alt="send message" class="h-6 w-6"/>
+		</button>
 </form></div>
