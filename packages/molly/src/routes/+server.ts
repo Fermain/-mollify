c99e6d418caf0ef2f0cbd3c940e50cdc7333b803
Megumi Kimura <kimuramegumi@gmail.com>
2023-09-13 16:14:26 +0200

    Uncomment +server and activate API

--- packages/molly/src/routes/+server.ts
+++ packages/molly/src/routes/+server.ts
@@ -1,6 +1,6 @@
-// import { OPENAI_TOKEN_LIMIT, OPENAI_API_KEY } from "$env/static/private";
-// import MollyAI from "$lib/server/openai";
+import { OPENAI_TOKEN_LIMIT, OPENAI_API_KEY } from '$env/static/private';
+import MollyAI from '$lib/server/openai';
 
-// const molly = new MollyAI(OPENAI_API_KEY, Number(OPENAI_TOKEN_LIMIT));
+const molly = new MollyAI(OPENAI_API_KEY, Number(OPENAI_TOKEN_LIMIT));
 
-// export const GET = molly.handlers.chatCompletion.GET;
+export const GET = molly.handlers.chatCompletion.GET;
