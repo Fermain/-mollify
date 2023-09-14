a659e9fdd66b7e9afc597548dc7d59798654b0d4
Megumi Kimura <64102873+MegumiKim@users.noreply.github.com>
2023-09-14 09:45:54 +0200

    Add POST method in +server.ts
    Co-authored-by: Stian Stordal <95475303+StianSto@users.noreply.github.com>

--- packages/molly/src/routes/+server.ts
+++ packages/molly/src/routes/+server.ts
@@ -3,4 +3,4 @@ import MollyAI from '$lib/server/openai';
 
 const molly = new MollyAI(OPENAI_API_KEY, Number(OPENAI_TOKEN_LIMIT));
 
-export const GET = molly.handlers.chatCompletion.GET;
+export const POST = molly.handlers.chatCompletion.POST;
