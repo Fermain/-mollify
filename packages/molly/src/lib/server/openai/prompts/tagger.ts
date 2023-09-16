a38ce812f2b0231d2505fd52f7a260946fac7244
Megumi Kimura <kimuramegumi@gmail.com>
2023-09-16 20:36:19 +0200

    Format molly package

--- packages/molly/src/lib/server/openai/prompts/tagger.ts
+++ packages/molly/src/lib/server/openai/prompts/tagger.ts
@@ -1,7 +1,7 @@
-export const tagger = (content: string) => (`
+export const tagger = (content: string) => `
 You are Tagger, an AI content tagger assistant that analyses written content and replies with a YAML encoded list of tags.
 You do not speak, except by providing a raw YAML array.
 
 Content:
 ${content}
-`);
+`;
--- packages/molly/src/lib/stores/readableStream.ts
+++ packages/molly/src/lib/stores/readableStream.ts
@@ -1,10 +1,10 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
 export function readableStreamStore() {
-	const { subscribe, set, update } = writable({ loading: false, text: "" });
+	const { subscribe, set, update } = writable({ loading: false, text: '' });
 
 	async function request(request: Request) {
-		set({ loading: true, text: "" });
+		set({ loading: true, text: '' });
 
 		try {
 			const response = await fetch(request);
@@ -13,20 +13,20 @@ export function readableStreamStore() {
 			if (!response.body) return;
 
 			const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
-			let result = "";
+			let result = '';
 
 			while (true) {
 				const { value: token, done } = await reader.read();
 
-				if (token != undefined) update((val) => {
-					result = val.text + token;
-					return ({ loading: false, text: result });
-				});
+				if (token != undefined)
+					update((val) => {
+						result = val.text + token;
+						return { loading: false, text: result };
+					});
 				if (done) break;
 			}
 
 			return result;
-
 		} catch (err: any) {
 			set({ loading: false, text: err.toString() });
 			throw err;
