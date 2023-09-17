5b4cf5636471cb3c8e558603d2a06f06ff3463a2
Ethmane Didi <ousman.didibela@gmail.com>
2023-09-17 10:19:09 +0200

    Updated fetchAudio function to make VOICE_ID and voice_settings optional with default values.

--- packages/tts/lib/fetchAudio.ts
+++ packages/tts/lib/fetchAudio.ts
@@ -2,15 +2,22 @@
  * Fetches audio from ElevenLabs API
  * @param {string} content Content string to be converted to audio
  * @param {string} ELEVENLABS_API_KEY Your ElevenLabs API key
- * @returns {Promise<Response>} Audio file
+ * @param {string} [VOICE_ID] Optional: Voice ID
+ * @param {Object} [voice_settings] Optional: Voice settings
+ * @param {number} [voice_settings.stability] Optional: Stability setting (default is 0)
+ * @returns {Promise<Response>} Response object representing the audio file
  */
 export async function fetchAudio(
   content: string,
-  ELEVENLABS_API_KEY: string
+  ELEVENLABS_API_KEY: string,
+  VOICE_ID?: string,
+  voice_settings?: { stability?: number }
 ): Promise<Response> {
-  const VOICE_ID = '21m00Tcm4TlvDq8ikWAM';
+  const defaultVoiceId = VOICE_ID || '21m00Tcm4TlvDq8ikWAM'; // Default value if not provided
+  const defaultStability = voice_settings?.stability ?? 0; // Default stability setting if not provided
+
   const response = await fetch(
-    `https://api.elevenlabs.io/v1/text-to-speech/${VOICE_ID}`,
+    `https://api.elevenlabs.io/v1/text-to-speech/${defaultVoiceId}`,
     {
       method: 'POST',
       headers: {
@@ -21,8 +28,8 @@ export async function fetchAudio(
       body: JSON.stringify({
         text: content,
         voice_settings: {
-          stability: 0,
-          similarity_boost: 0,
+          stability: defaultStability,
+          similarity_boost: 0, // You can provide a default value for this property as well if needed in the future!
         },
       }),
     }
