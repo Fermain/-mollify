664ad3d48ba5d420cf5e9e88dd1724431122d414
Megumi Kimura <64102873+MegumiKim@users.noreply.github.com>
2023-09-29 15:45:14 +0200

    Update packages/lms/src/lib/components/ui/Icon.svelte
    Co-authored-by: Oliver Dipple <Fermain@users.noreply.github.com>

--- packages/lms/src/lib/components/ui/Icon.svelte
+++ packages/lms/src/lib/components/ui/Icon.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let name: string;
-  export let variant:string = "";
+  export let variant = "";
   import 'material-icons/iconfont/material-icons.css';
 </script>
 <!-- Default variant is "filled". Please give variant type (outlined/round/sharp/two-tone) as prop if you want to change-->
