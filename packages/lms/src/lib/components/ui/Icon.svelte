88a3d0731f11617cead732faa54b12257b5c7e1b
Megumi Kimura <kimuramegumi@gmail.com>
2023-09-29 14:54:56 +0200

    Add default variant (empty string) and comment

--- packages/lms/src/lib/components/ui/Icon.svelte
+++ packages/lms/src/lib/components/ui/Icon.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   export let name: string;
-  export let variant:string;
+  export let variant:string = "";
   import 'material-icons/iconfont/material-icons.css';
 </script>
-
+<!-- Default variant is "filled". Please give variant type (outlined/round/sharp/two-tone) as prop if you want to change-->
+<!-- https://www.npmjs.com/package/material-icons#usage -->
 <span class={variant? `material-icons-${variant}`: 'material-icons'} {...$$restProps}>{name}</span>
