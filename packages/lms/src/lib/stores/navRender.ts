17c71bb77e989a4e4ee916af767f678cade0e36a
Fermain <Fermain@users.noreply.github.com>
2023-10-04 16:04:21 +0200

    Formatting

--- packages/lms/src/lib/stores/navRender.ts
+++ packages/lms/src/lib/stores/navRender.ts
@@ -1,3 +1,3 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
-export const navRenderParamStore = writable('default')
+export const navRenderParamStore = writable('default');
