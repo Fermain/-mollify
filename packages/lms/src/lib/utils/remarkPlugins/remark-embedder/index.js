6153a7ef180ec114fa1017d17c7e32f0853a8d19
Christopher <46559129+Christonn93@users.noreply.github.com>
2024-02-28 11:03:45 +0100

    Removing code that is not in use.

--- packages/lms/src/lib/utils/remarkPlugins/remark-embedder/index.js
+++
@@ -1,4 +0,0 @@
-import iframePlugin from './iframePlugin.js';
-
-// Use the plugin
-const processor = unified().use(iframePlugin());
--- packages/lms/src/lib/utils/remarkPlugins/svelte-remark-embed-video/helpers.js
+++
@@ -1,7 +0,0 @@
-module.exports.addDefaultImport = function addImport(tree, mod, name) {
-    tree.children.unshift({
-      type: 'import',
-      value: `import _${name} from '${mod}'`,
-    });
-    return `_${name}`;
-  };
