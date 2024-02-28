1e9d7335f42f5ca1226a404253953c1dbced9c84
Christopher <46559129+Christonn93@users.noreply.github.com>
2024-02-28 11:58:43 +0100

    adjusting code

--- packages/lms/src/lib/utils/remarkPlugins/remark-embedder/iframePlugin.js
+++ packages/lms/src/lib/utils/remarkPlugins/remark-embedder/iframePlugin.js
@@ -1,7 +1,28 @@
-import transformer from './transformer.js';
+import { visit } from 'unist-util-visit';
 
 function iframePlugin() {
-  return transformer;
+  return (tree) => {
+    visit(tree, 'text', (node, index, parent) => {
+      if (node.type === 'text' && node.value === '@') {
+        // Getting node value
+        const nodeValue = node.value;
+
+        // Finding link in child
+        const linkObject = parent.children.find((child) => child.type === 'link');
+
+        if (nodeValue === '@' && linkObject) {
+          // console.log('NodeValue: ', nodeValue);
+
+          // Setting the url
+          const url = parent.children[1].url;
+          // console.log('Url:', url);
+
+          node.type = 'html';
+          node.value = `<iframe src="${url}" style="aspect-ratio: 16 / 9; width: 100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
+        }
+      }
+    });
+  };
 }
 
 export default iframePlugin;
