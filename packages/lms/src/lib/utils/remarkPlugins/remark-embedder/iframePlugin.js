a8b690ec45ba317d3d1761a851f46ca35f8742df
Christopher <46559129+Christonn93@users.noreply.github.com>
2024-02-28 12:31:42 +0100

    update.

--- packages/lms/src/lib/utils/remarkPlugins/remark-embedder/iframePlugin.js
+++ packages/lms/src/lib/utils/remarkPlugins/remark-embedder/iframePlugin.js
@@ -11,14 +11,29 @@ function iframePlugin() {
         const linkObject = parent.children.find((child) => child.type === 'link');
 
         if (nodeValue === '@' && linkObject) {
-          // console.log('NodeValue: ', nodeValue);
-
           // Setting the url
+          // console.log('NodeValue: ', nodeValue);
           const url = parent.children[1].url;
           // console.log('Url:', url);
 
-          node.type = 'html';
-          node.value = `<iframe src="${url}" style="aspect-ratio: 16 / 9; width: 100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
+          if (parent.children[1].url.startsWith('https://www.youtube.com')) {
+            // Create a URL object
+            const videoId = new URL(url);
+
+            // Get the value of the 'v' parameter
+            const youtubeId = videoId.searchParams.get('v');
+
+            // Setting youtube url as embed video
+            const youtubeVideoUrl = `https://www.youtube.com/embed/${youtubeId}`;
+
+            // Returning html if youtube video
+            node.type = 'html';
+            node.value = `<iframe src="${youtubeVideoUrl}" style="aspect-ratio: 16 / 9; width: 100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
+          } else {
+            // Returning html if not youtube video
+            node.type = 'html';
+            node.value = `<iframe src="${url}" style="aspect-ratio: 16 / 9; width: 100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
+          }
         }
       }
     });
